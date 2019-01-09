ed3baca19e6c535b92a498e7e95e08337952f2ab
Angelos Lechouritis <angelos.lexouritis@gmail.com>
2019-01-09 12:28:18 +0200

    added click for every article and extracted it's content also
    still work needed to be done in content, in order to clear it from html

--- NLP_search_engine/spiders/texts_spider.py
+++ NLP_search_engine/spiders/texts_spider.py
@@ -1,25 +1,31 @@
 import scrapy
 
+
 class TextsSpider(scrapy.Spider):
-    name = "texts"  
+    name = "texts"
     start_urls = [
         'https://thehackernews.com/'
-    ] 
-    
-    """  for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse) """
+    ]
 
     # Executed for every url specified in url - just example to begin with
-    # to-do: "click" every post and get its content, also "click" every next-page and get all texts same way
     def parse(self, response):
-        """  print("response")
-        print(response) """
+        # "click" every article and run parse_text_data function
         for text in response.css('div.body-post'):
-            yield {
-                'text-title': text.css('h2.home-title::text').extract_first(),
-                'text-url': text.css('a.story-link::attr(href)').extract_first()
-            }
-        
-        next_page = response.css('a.blog-pager-older-link-mobile::attr("href")').extract_first()
+            text_content = text.css(
+                'a.story-link::attr("href")').extract_first()
+            if text_content is not None:
+                yield response.follow(text_content, self.parse_text_data)
+        # get next page's articles and rerun parse for each of them
+        next_page = response.css(
+            'a.blog-pager-older-link-mobile::attr("href")').extract_first()
         if next_page is not None:
             yield response.follow(next_page, self.parse)
+    # Gets the title, url and content for each article
+    # to-do: remove all html and parse only the clean text of the articles
+    def parse_text_data(self, response):
+        for text in response.css('div.main-box'):
+            yield {
+                'text-title': text.css('a::text').extract_first(),
+                'text-url': text.css('a::attr(href)').extract_first(),
+                'text-content': text.css('div.articlebody').extract()
+            }
