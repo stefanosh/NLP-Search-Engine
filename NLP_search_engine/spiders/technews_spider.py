d90deab0d8e50b982e0b719104fa0b8a3be2ec77
Stefanos Panagiotou <stefanoshzc@gmail.com>
2019-01-15 23:37:57 +0200

    updated content preprocessing with lxml just as texts_spider

--- NLP_search_engine/spiders/technews_spider.py
+++ NLP_search_engine/spiders/technews_spider.py
@@ -1,12 +1,13 @@
 import scrapy
-from bs4 import BeautifulSoup
+import lxml.etree
+import lxml.html
 
 class TechnewsSpider(scrapy.Spider):
     name = "technews"
     start_urls = [
         'https://www.technewsworld.com/'
     ]
-  
+    
     # Executed for every url specified in url - just example to begin with
     def parse(self, response):
         domain = "https://www.technewsworld.com"
@@ -16,7 +17,8 @@ class TechnewsSpider(scrapy.Spider):
             text_content = text.css('a::attr("href")').extract_first() 
             if text_content is not None:
                 text_content = domain + text_content
-                yield response.follow(text_content, self.parse_text_data)        
+                yield response.follow(text_content, self.parse_text_data)    
+
         # get next page's articles and rerun parse for each of them
         next_page = response.css(
             '#earlier a::attr("href")').extract_first()
@@ -25,14 +27,27 @@ class TechnewsSpider(scrapy.Spider):
             yield response.follow(next_page, self.parse)
 
     # Gets the title, url and content without html tags and newline characters for each article
-    # to-do: remove all javascript code found in every article such as 
-    #        " <!--//<![CDATA[//]]>//--><!--//<![CDATA[\r(adsbygoogle = window.adsbygoogle || []).push({});\r//]]>//-->"
-    #        remove '\"'
+    # to-do: do not include content about author
+    #        Specifically, ignore <p id="story-authorbio">
+
+   
     def parse_text_data(self, response):
         for text in response.css('#story'):          
             content = text.css('#story-body').extract_first()
-            soup = BeautifulSoup(content, 'html.parser')
-            just_text = soup.get_text().replace("\n", "") 
+            root = lxml.html.fromstring(content)
+
+            # remove tags that are not usually rendered in browsers
+            # javascript, HTML/HEAD, comments, add the tag names you dont want at the end
+            lxml.etree.strip_elements(
+                root, lxml.etree.Comment, "script", "head")
+            
+            #convert html to string
+            just_text = lxml.html.tostring(
+                root, method="text", encoding="unicode")
+
+            #remove newline chars
+            just_text = just_text.replace("\n", " ").replace('\"', " ").replace("\t", " ")
+                     
             yield {
                 'text-title': text.css('h1.title::text').extract_first(),
                 'text-url': response.request.url,
