5d8f9c94e1ef8a4bec2642552feae1b75fccea09
Angelos <angelos.lexouritis@gmail.com>
2019-01-17 13:33:10 +0200

    tf_idf implemented

--- NLP_search_engine/vector_space__model.py
+++ NLP_search_engine/vector_space__model.py
@@ -1,8 +1,39 @@
 import json
 from nltk.stem import WordNetLemmatizer
+import numpy as np
+# calculates TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document)
+# article is in json format, containing all the lemmas
+
+
+def tf_of(word, article):
+    times_t_in_article = 0
+    total_words_in_article = 0
+    for lemma in article:  # for every lemma in article
+        if word == lemma["word"]:
+            times_t_in_article += 1
+        total_words_in_article += 1
+    return(times_t_in_article / float(total_words_in_article))
+
+# calculates IDF(t) = log_e(Total number of documents / Number of documents with term t in it)
+
+
+def idf_of(word, data):
+    total_number_of_docs = 0
+    number_of_docs_containing_word = 0
+    for article in data["articles"]:
+        for text_id in article:
+            # for every word in every article
+            for lemma in article[str(text_id)]:
+                if(lemma["word"] == word):
+                    number_of_docs_containing_word += 1
+                    break
+        total_number_of_docs += 1
+    return np.log(total_number_of_docs / float(number_of_docs_containing_word))
 
 
 # for every article it removes the stopwords, by creating a new array which holds all the values of
+
+
 def removeStopWords(data):
     closedClassCategoriesTuple = ("CD", "CC", "DT", "EX", "IN", "LS", "MD", "PDT",
                                   "POS", "PRP",  "PRP",  "RP",   "TO", "UH", "WDT", "WP", "WP", "WRB")
@@ -40,4 +71,18 @@ for article in data["articles"]:
             word["word"] = lemmatizer.lemmatize(
                 word["word"], pos=word["pos_tag"].lower())
 
+# calculate tf for every lemma and add it to json object
+for article in data["articles"]:
+    for text_id in article:
+            # for every word in every article
+        for word in article[str(text_id)]:
+            word["tf"] = tf_of(word["word"], article[str(text_id)])
 
+# calculate idf for every lemma and add it to json object
+for article in data["articles"]:
+    for text_id in article:
+            # for every word in every article
+        for word in article[str(text_id)]:
+            word["idf"] = idf_of(word["word"], data)
+            word["tf_idf"] = word["idf"] * word["tf"]
+print(data)
